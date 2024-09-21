c6cd7ea039ebeb218d744b65b9018520175ab617
tulin.design <t.tulin@mail.ru>
2024-09-22 00:01:36 +0500

    Операторы присваивания, сравнения и порядок операторов

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-// Базовые арифметические операторы
-const width = 10;
-const height = 5;
-const space = width * height;
-const newWidth = width - 4;
-const newWidth2 = width + 4;
-const division = newWidth / newWidth2;
-const volume = 2 ** 3; // 2 * 2 * 2
-console.log(volume);
+// Операторы присваивания
+let age = 18 + 5;
+age += 2; // age = age + 2
+age -= 3; // age = age - 2
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
 
-// Строки
-const city = 'Москва';
-const street = 'Новослободская';
-console.log(city + ', ' + street + ' ' + 5);
+age++; // age = age + 1
+age--; // age = age - 1
+
+console.log(age);
+
+// Операторы сравнения
+const vasia = 20;
+console.log(age > vasia); // 22 > 20 = true
+console.log(age >= vasia); // 22 > 20 = true
+console.log(age < vasia); // 22 > 20 = false
+console.log(age <= vasia); // 22 > 20 = false
+console.log(age == vasia); // 22 == 20 = false
+
+
+// Порядок операторов
+const isSuited = 100 - 10 > 90 - 5; // true
+console.log(isSuited);
+
+// 100 - (12приоиритет) 10 > (10приоиритет) 90 - (12приоиритет) 5
+
+
+const a = (6 + 10) / 2
+console.log(a);
+
+let b;
+let c;
+c = b = 100 + 50;
+console.log(c);
+console.log(b);
