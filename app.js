3044f38327c8fc8c177365423f05cc39a5d6941a
tulin.design <t.tulin@mail.ru>
2024-09-22 00:32:00 +0500

    Типы данных

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
-// Операторы присваивания
-let age = 18 + 5;
-age += 2; // age = age + 2
-age -= 3; // age = age - 2
-age *= 2; // age = age * 2
-age /= 2; // age = age / 2
+// Типы данных
+let a = 5;
+let b = 5.6;
 
-age++; // age = age + 1
-age--; // age = age - 1
+console.log(typeof a);
+console.log(typeof b);
 
-console.log(age);
+a = 'd';
 
-// Операторы сравнения
-const vasia = 20;
-console.log(age > vasia); // 22 > 20 = true
-console.log(age >= vasia); // 22 > 20 = true
-console.log(age < vasia); // 22 > 20 = false
-console.log(age <= vasia); // 22 > 20 = false
-console.log(age == vasia); // 22 == 20 = false
+console.log(typeof a);
 
+let isAdmin = a > 10;
+console.log(typeof isAdmin);
 
-// Порядок операторов
-const isSuited = 100 - 10 > 90 - 5; // true
-console.log(isSuited);
-
-// 100 - (12приоиритет) 10 > (10приоиритет) 90 - (12приоиритет) 5
-
-
-const a = (6 + 10) / 2
-console.log(a);
-
-let b;
 let c;
-c = b = 100 + 50;
-console.log(c);
-console.log(b);
+console.log(typeof c);
+c = 5;
+console.log(typeof c);
+
+let d = null;
+console.log(typeof d == null);
+
+
+ 
