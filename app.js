b0fbe851fb2f1576694f4540b8d2574c3c5fe4b5
tulin.design <t.tulin@mail.ru>
2024-09-21 23:46:49 +0500

    Базовые арифметические операторы

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+// Базовые арифметические операторы
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume);
+
+// Строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ' ' + 5);
