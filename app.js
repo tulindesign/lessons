edc78b61d8415cab613ccd0217076150852c84ec
tulin.design <t.tulin@mail.ru>
2024-09-22 22:35:32 +0500

    Упражнение - проверка робота

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
-// Тернарные операторы
-const bmwX3Price = 100000;
-const fordFocusPrice = 10000;
-const budget = 20000;
+//Проверка робота
 
-/*let message;
-if (budget > bmwX3Price) {
-     message = "BMW";
+/* Меотодом promt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15". 
+Если ответ верен введите в консоль "Успех", 
+если нет - "Вы робот!", 
+а если он введет "Я не робот", то тоже успех'
+
+*/
+
+const question = prompt('Сколько будет 7 + или -15?'); // 7+15 = 22, 7-15 = -8
+
+/*
+const message =  
+question == 22 // if
+? "Успех" // вариант 1
+: question == -8  // else if
+? "Успех"  // вариант 2
+: question == 'Я не робот' // else if
+? "Успех"  // вариант 3
+: 'Вы робот!' // else
+console.log(`${message}`);
+
+*/
+
+
+if (question === 'Я не робот') {
+    console.log('Успех');
 } else {
-     message = 'Велосипед';
+    const resNumber = Number(question);
+    switch(resNumber) {
+        case 22:
+        case -8:
+            console.log('Успех');
+            break;
+        default:
+            console.log('Ты робот!');
+    }
+
 }
-const message = budget > bmwX3Price ? message = "BMW" : message = 'Велосипед'
-console.log (`Я хочу купить ${message}`)*/
 
 
-// Тернарные операторы
 
-const send =  
-budget > bmwX3Price // if
-? "BMW" // вариант 1
-: budget > fordFocusPrice  // else if
-? "Ford"  // вариант 2
-: 'Велосипед' // else
+const message =  
+question == 22 // if
+? "Успех" // вариант 1
+: question == -8  // else if
+? "Успех"  // вариант 2
+: question == 'Я не робот' // else if
+? "Успех"  // вариант 3
+: 'Вы робот!' // else
+console.log(`${message}`);
 
 
-console.log (`Я хочу купить ${send}`)
+//доп тест
+let serverdata = null
+const defaultimg = serverdata === null ? 'default' : 'загрузилась дата';
+
+console.log(`${defaultimg}`);
+
+
+//доп тест
+let balance = 600;
+console.log(`${balance>1000 
+    ? 'Круто баланса хватит' 
+    : balance < 599 
+    ? 'Баланса мало'
+    : 'ни на че не хватит'
+}`)
