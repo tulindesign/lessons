22c251cc435ad1b6d197b1f6e89fc2e23c35acc0
tulin.design <t.tulin@mail.ru>
2024-09-23 10:51:15 +0500

    Логические операторы

--- app.js
+++ app.js
@@ -1,68 +1,31 @@
-//Проверка робота
+//Логические операторы
+// И - когда ВСЕ true
+// ИЛИ - когда хотябы 1 true
 
-/* Меотодом promt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15". 
-Если ответ верен введите в консоль "Успех", 
-если нет - "Вы робот!", 
-а если он введет "Я не робот", то тоже успех'
-
-*/
-
-const question = prompt('Сколько будет 7 + или -15?'); // 7+15 = 22, 7-15 = -8
-
+const isAdmin = true;
+const canWrite = true;
+let superAdmin;
 /*
-const message =  
-question == 22 // if
-? "Успех" // вариант 1
-: question == -8  // else if
-? "Успех"  // вариант 2
-: question == 'Я не робот' // else if
-? "Успех"  // вариант 3
-: 'Вы робот!' // else
-console.log(`${message}`);
+console.log(`Есть права на редактирование системных файлов ${isAdmin && canWrite ? 'Да' : 'Нет'}`); // И
 
+
+console.log(`Могу редактировать обычный файл (либо админ, либо есть права на редактирование) ${isAdmin || canWrite ? 'Да' : 'Нет'}`); // ИЛИ
+
+
+console.log(`Все остальные пользователи НЕ админы - ${!isAdmin ? 'Да' : 'нет'}`); // Инверсия
 */
 
+console.log(`Системный файл;
+Есть доступ к редактированию -  ${canWrite == true ? 'Да': 'Нет'}`);
 
-if (question === 'Я не робот') {
-    console.log('Успех');
+let isEdited = prompt('Файл редактируется? Да (1) или Нет(0)');
+
+if (isEdited == 1) {
+    SuperAdmin = prompt('Вы супер админ? Да(1) Нет (0)');
+
+    console.log(SuperAdmin == 1 ? `Можно редактировать любой файл, т.к. вы СуперАдмин` : `На текущий момент редактировать - ${isAdmin && canWrite && Boolean(!(isEdited == 1 ? true : false)) ? 'Можно' : 'Нельзя'}`);
+
+
 } else {
-    const resNumber = Number(question);
-    switch(resNumber) {
-        case 22:
-        case -8:
-            console.log('Успех');
-            break;
-        default:
-            console.log('Ты робот!');
-    }
-
+    console.log(`На текущий момент редактировать - ${isAdmin && canWrite && Boolean(!(isEdited == 1 ? true : false)) ? 'Можно' : 'Нельзя'}`);
 }
-
-
-
-const message =  
-question == 22 // if
-? "Успех" // вариант 1
-: question == -8  // else if
-? "Успех"  // вариант 2
-: question == 'Я не робот' // else if
-? "Успех"  // вариант 3
-: 'Вы робот!' // else
-console.log(`${message}`);
-
-
-//доп тест
-let serverdata = null
-const defaultimg = serverdata === null ? 'default' : 'загрузилась дата';
-
-console.log(`${defaultimg}`);
-
-
-//доп тест
-let balance = 600;
-console.log(`${balance>1000 
-    ? 'Круто баланса хватит' 
-    : balance < 599 
-    ? 'Баланса мало'
-    : 'ни на че не хватит'
-}`)
