56319c6dd20bb9ce7d740074d8200719c5061880
tulin.design <t.tulin@mail.ru>
2024-09-23 11:50:38 +0500

    Оператор нулевого слияния

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-// // Операторы с другими типами
+// Оператор нулевого слияния
 
-// console.log('Вася' || 'Олег'); // Вася, т.к ПЕРВЫЙ и если 
-// console.log(false || 'Олег'); // Олег, т.к. первый false 
-// console.log('Вася' || false); // Вася, т.к. первый / true
-// console.log(false || false); //false
-
-
-// console.log('Вася' && 'Олег'); // Олег, т.к. он последний 
-// console.log(false && 'Олег'); //false, т.к. есть хотябы 1 false
-// console.log(false && 'Олег'); //false, т.к. есть хотябы 1 false
-// console.log(false && false); //false
+let userName = '';
+console.log(userName || 'Гость'); //игнорит 0, '', null, underfined)
+console.log(userName ?? 'Гость');
 
 
-let a;
-const userName = a || 'Петя';
-console.log(userName); // Петя, т.к. a = undefined
-
-const isAdmin = prompt('Вы админ? Да(1) Нет (0)');
- 
-
-const fileName = (isAdmin == Boolean(1) ? true : false) && 'file.mp4';
-console.log(fileName); // file.mp4 
+let age = 0;
+console.log(age || '18');//игнорит 0, '', null, underfined)
+console.log(age ?? '18'); // 0
