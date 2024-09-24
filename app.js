3c62f06cc622cb2ca7876a387c4a5fa068c8f4a5
tulin.design <t.tulin@mail.ru>
2024-09-24 23:45:54 +0500

    Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-// Переписать функции в стрелочную 
+// Параметры по умолчанию
 
-function toPower(num, power) {
+// function toPower(num = 4, power = 5) {
+//     const res = num ** power;
+//     return res;
+// }
+// console.log(toPower(2,3));
+// console.log(toPower(2));
+
+// const num = num ?? 10 - значение 10 если undefined или null
+
+
+function toPower(num = prompt('Какое у вас число?'), power = prompt('В какую степерь возвести?')) {
     const res = num ** power;
     return res;
 }
 console.log(toPower(2,3));
-
-const toP = (num, power) => num ** power;
-console.log(toP(2,3));
+console.log(toPower(22));
+console.log(toPower());
