0e49f934ebb8eb09f4bf5c12bef9073745f73e5c
tulin.design <t.tulin@mail.ru>
2024-09-24 23:22:36 +0500

    Анонимные функции

--- app.js
+++ app.js
@@ -1,60 +1,13 @@
-// // Введение в функции
-// function fromMilesToKm(miles) {
-//     const km = miles / 0.62137;
-//     return km;
-// }
+// Анонимные функции
 
-// /* 
-// Ключевое слово - function
-// Название fromMilesToKm
-// Аргумент(ы) ()
-// Возвращаемое значение return переменна;
-// */
-
-// console.log(fromMilesToKm(100));
+console.log(powerOfTwo(2));
+function powerOfTwo(num) {
+    return num ** 2 ;
+} // можем использовать ДО самой объявления самой фукнции в коде
 
 
-// function logName(firstname, surname) {
-//     console.log(`Мое имя ${firstname}
-// Моя фамилия ${surname}`);
-// }
-// logName(firstname = prompt('Ваше имя?'),surname = prompt('Ваше фамилия?')); 
-
-// function logName(firstname, surname) {
-//     console.log(`Мое имя ${firstname}
-// Моя фамилия ${surname}`);
-// }
-// logName(prompt('Ваше имя?'),prompt('Ваше фамилия?')); 
-
-
-// function countDepositSum(depositInUsd, month, rate) {
-//     return sum = depositInUsd * (1 + (rate/100)/ 12) ** month;
-// }
-// console.log(countDepositSum(1000,24,7));
-// console.log(countDepositSum(2000,12,14));
-// console.log(countDepositSum(3000,24,7));
-
-
-
-
-// function countDepositSum(depositInUsd, month, rate) {
-//     const sum = depositInUsd * (1 + (rate/100)/12) ** month;
-//     console.log(`Сумма через ${month} месяца, с входными ${depositInUsd}$ и ставкой ${rate}% годовых = ${sum}$ `);
-//     return sum;
-
-// }
-// const countedDepositSum = countDepositSum(prompt('Сколько у вас денег'),prompt('Срок вклада в месяцах'),prompt('Ставка в %'));
-
-
-
-function countDepositSum(depositInUsd, month, rate) {
-    const sum = depositInUsd * (1 + (rate/100)/12) ** month;
-    return sum;
+const poft = function (num) {
+    return num ** 2 ;
 }
-const depositInUsd = prompt('Сколько у вас денег');
-const month = prompt('Срок вклада в месяцах');;
-const rate = prompt('Ставка в %');
 
-console.log(`Сумма через ${month} месяца, с входными ${depositInUsd}$ и ставкой ${rate}% годовых = ${countDepositSum(month,depositInUsd,rate)}$`);
-
-
+console.log(poft(4)); //использщвание только ПОСЛЕ объявления переменной функции)
