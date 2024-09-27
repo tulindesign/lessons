6058cacaad7e3cfa4d19ca7d7ee1dcfd477fc3f6
tulin.design <t.tulin@mail.ru>
2024-09-27 09:33:34 +0500

    Поиск элемента в массиве

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-// Управление элементами массива
-const users = ['Аня','Вика','Катя'];
-users[users.length -1] = 'Кристина'; // перезаписали 3 элемент массива
-console.log(users[users.length -1]);
+//поиск элемента в массиве
+
+const roles = ['user','admin','manager','admin'];
+
+const elIndex =  roles.indexOf('admin') // ищет первое вхождение
+console.log(elIndex);
 
 
-users[users.length] = 'Никита'; // добавили никиту последним
-console.log(users[users.length -1]);
+const elIndex2 =  roles.indexOf('superuser') // поиск, не найдено -1
+console.log(elIndex2);
+
+const question = prompt('Ваша роль?');
+const searchResult = roles.indexOf(String(question)) >=0 ? 'Доступ разрешен': 'нет доступа, роль не найдена';
+console.log(searchResult);
+// name.indexOf - проверяет наличие, индекс, или -1 если не найден
 
 
-const arrLenght = users.push('Игорь'); // добавить последним
-console.log(users);
-console.log(arrLenght);
-
-
-const arrLenght2 = users.unshift('Тимофей'); // добавить первым
-console.log(users);
-console.log(arrLenght2);
-
-
-users.pop(); // удалил последний
-const arrLenght3 = users.pop();  // в отличии от добавления, возвращает удаленный элемент
-console.log(users);
-console.log(arrLenght3);
-
-
-const el2 = users.shift();  // удалил последний
-// в отличии от добавления, возвращает удаленный элемент
-console.log(el2);
-console.log(users);
+const question2 = prompt('Ваша роль?');
+const searchResult2 = roles.includes(question2) ? 'Доступ разрешен': 'нет доступа, роль не найдена';
+console.log(searchResult2);
+// name.includes - проверяет наличие, возвращает true
