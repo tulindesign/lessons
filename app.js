bc571ded1aed3c06b3520ddda5791897f5ea4b69
tulin.design <t.tulin@mail.ru>
2024-09-30 10:25:53 +0500

    Упражнение - разбор URL

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-// Оператор REST
+// Упражнение - функция разрбота URL
 
-// ...name = ОСТАЛЬНОЕ (только до конца)
+/* 
+    Дан произвольный url
+    'https://purpleschool.ru/course/javascript'
 
-const data =[1,2,3,4,5,6]
-const [one,two, ...others] = data;
-console.log(one,two, others);
+    Нужно сделать функция, которая будет выводить в консоль
+    - Протокол
+    - Доменное имя
+    - Путь внутри сайта
+
+*/
+
+
+let getUrlPrompt = prompt('Вставьте ваш URL');
+while (!getUrlPrompt.includes('http')) {
+    getUrlPrompt = prompt('Не правильная ссылка. Вставьте другой URL');
+}getPartsUrl(getUrlPrompt);
+
+
+function getPartsUrl(url) {
+    const [protocol, _1, domain, ...siteWay] = url.split('/');
+    const getFullLink = [protocol,'/',domain,...siteWay];
+
+    console.log(`Ваша ссылка 
+        ${url}
+        
+        Протокол: 
+        ${protocol}
+        
+        Домен: 
+        ${domain}
+        
+        Путь на сайте: 
+        /${siteWay.join('/')}
+        
+        И снова ваша ссылка:
+        ${getFullLink.join('/')}
+        `)
+}
+
+
+
+
+
+
+
+
