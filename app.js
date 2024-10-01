1682ab1fe80b7ef99babece43877ee0e04c4dbac
tulin.design <t.tulin@mail.ru>
2024-10-01 10:18:20 +0500

    break и continue

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-// Цикл for 
-for(let i = 1; i<=10; i++) {
-console.log(`Ваш баланс ${i}$`);
-}
+// break и continue
+const tasks = ['Задача 1', 'continue','Задача 3','Задача 4','break','Задача 6'];
+
+
+for(let i=0; i < tasks.length; i++) {
+    if ((tasks[i]) == 'continue') {
+        continue;
+    } else if ((tasks[i]) == 'break') {
+       break; 
+    }
+    console.log(tasks[i]);       
+} 
