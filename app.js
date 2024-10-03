e1490101c4951d8d997254498be8b3d9ff233e70
tulin.design <t.tulin@mail.ru>
2024-10-03 22:32:52 +0500

    Цикл while + Continue

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-// Цикл while
-let i = 1;
-while (i<=10) { // Проверка ДО
-    console.log(`Вывод - ${i}`);
-    i++;
+// // Цикл while
+// let i = 1;
+// while (i<=10) { // Проверка ДО
+//     console.log(`Вывод - ${i}`);
+//     i++;
+// }
+
+
+// const arr = [1,4,8,7]
+// let j=0;
+// while (arr[j] <= 5 && j < arr.length) { // Проверка ДО
+//     console.log(arr[j]);
+//     j++;
+// }
+
+// let k = 0;
+// do { // Проверка ПОСЛЕ 1 выполнения
+// console.log(`k - ${k}`)
+// k++
+// } while( k < 0)
+
+
+
+// 
+console.log('---------------');
+
+const arr2 = [1,4,8,7,12,15,16];
+let q = -1;
+while (q < arr2.length) { // Проверка ДО
+   q++;
+    if (arr2[q] == 8) {
+      console.log('пропустил 8');
+      continue;
+     } 
+     console.log(arr2[q]);
 }
-
-
-const arr = [1,4,8,7]
-let j=0;
-while (arr[j] <= 5 && j < arr.length) { // Проверка ДО
-    console.log(arr[j]);
-    j++;
-}
-
-let k = 0;
-do { // Проверка ПОСЛЕ 1 выполнения
-console.log(`k - ${k}`)
-k++
-} while( k < 0)
