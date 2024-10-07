bb08e9557d3d4c77b373780856e65f23bd97008f
tulin.design <t.tulin@mail.ru>
2024-10-07 23:57:18 +0500

    Пример стрелочной функции

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-function power(pow) {
-    return function(num) {
-        return num**pow;
-    }
-}
+let ask = (question, yes, no) => confirm(question)?yes():no();
 
 
-
-const powerOfTwo = power(2);
-console.log(powerOfTwo(5));
-
-
-
-const powerOfThree = power(3);
-console.log(powerOfThree(5));
-
-
-console.log(power(5)(4));
-
-
-
-
-let abcd = (a,b) => (c,d) => (a+b)*(c+d);
-console.log(abcd(2,2)(1,1));
+ask(
+  "Вы согласны?",
+  ()=>console.log("Вы согласились."),
+  ()=>console.log("Вы отменили выполнение.")
+);
