a0c54b6ac0958e010e879d89a176a4fb28c89760
tulin.design <t.tulin@mail.ru>
2024-10-08 22:13:46 +0500

    Упражнение - среднее значение (reduce)

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-const operations = [100, -20, 7, -30, 50];
-// let balance = 0;
-// for (let operation of operations) {
-//     balance += operation;
-// }
-// console.log(balance);
+
+/* 
+Найти среднее значение последовательных чисел с помощью reduce
+*/
+const arr = [1,4,4,10]
 
 
+const averageValueOfArray = arr.reduce((sum,value,i) => {
+   if (i != arr.length - 1) { // если не последний
+        return sum + value; 
+   } return (sum + value)/(i+1); //последний - сумма/кол-во элементов
+}, 0);
 
-// acc - темп переменная куда складывается операция
-// value - текущее значение массива
-// i - номер операции
-const finalBalance = operations.reduce((acc,value, i) => {
-    console.log(`Операция ${i+1}, Текущий баланс: ${acc}, Операция: ${value}$`);
-    return acc += value;
-}, 0); // значение по умолчанию (аля let finalBalance = 0)
-
-console.log(`Итого: ${finalBalance}$`);
-
-
-const minElement = operations.reduce((min,value, i) => {
-    return min < value ? min: min = value;
-}, 0); // значение по умолчанию (аля let finalBalance = 0)
-
-console.log(`Минимальный элемент: ${minElement}$`);
+console.log(`Среднее значение массива: ${averageValueOfArray}$`);
