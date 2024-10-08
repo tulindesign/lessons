b3c5ef0dd68072e6dff19fcf1a44a4d424063e07
tulin.design <t.tulin@mail.ru>
2024-10-08 22:56:48 +0500

    flat, flatmap

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-/* 
-    Написать функцию, которые возвращает true,
-    если элемент есть и false, если нет.
-*/
-const arr = [2,4,4,10,20,30,40,50]
-
-// function some(array) {
-//     questValue = +(prompt('Поиск по числу'));
-
-//     if (array.find((element) => element === questValue)) {
-//         return true;
-//     } return false;
-// }
-
-
-// console.log(some(arr));
-
-
-
-
-// function some2(array,element) {
-//     element = +element;
-//     const res = array.find(el => el === element);
-//     return res !== undefined ? true: false;
-// }
-// console.log(some2(arr,prompt('Поиск по числу')));
-
-
-console.log(arr.some(el => el === 0));
+const prices = [[2,4], [3,4], [10, [20, 50]]];
+const res = prices.flat(10); // сначала flat
+const res2 = prices.flatMap((el,i) => `${i}.${el}`); //сначала map потом flat
+console.log(res);
+console.log(res2);
