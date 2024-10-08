0037e6c9e8f0cc8dea6b30b663ac110cfe457839
tulin.design <t.tulin@mail.ru>
2024-10-08 22:27:31 +0500

    find, findIndex

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 
-/* 
-Найти среднее значение последовательных чисел с помощью reduce
-*/
-const arr = [1,4,4,10]
+const arr = [1,2,4,4,10,20,30,40,50]
+
+let elGT5;
+
+// for (const el of arr) {
+//     if (el > 5) {
+//     elGT5 = el;
+//     break;
+// }
+// }
 
 
-const averageValueOfArray = arr.reduce((sum,value,i) => {
-   if (i != arr.length - 1) { // если не последний
-        return sum + value; 
-   } return (sum + value)/(i+1); //последний - сумма/кол-во элементов
-}, 0);
 
-console.log(`Среднее значение массива: ${averageValueOfArray}$`);
+elGT5 = arr.find((el) => el  > 5);
+console.log(elGT5);
+ 
+elGT5 = arr.findIndex((el) => el  > 5);
+console.log(elGT5);
