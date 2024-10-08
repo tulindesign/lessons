dd31466fb381f026d0931544980233a54de30d18
tulin.design <t.tulin@mail.ru>
2024-10-08 11:31:52 +0500

    reduce

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-/*
-    Имеется массив изменения цен prices,
-    где внутри
-    1 элемент - цена в момент X
-    2 элемент - цена в момент Y
+const operations = [100, -20, 7, -30, 50];
+// let balance = 0;
+// for (let operation of operations) {
+//     balance += operation;
+// }
+// console.log(balance);
 
-    Нужно преобразовать данные в массив, где будут отображены только положительные изменения цен [100,150]
-*/
 
-const prices =[[100,200],[120,100],[200,350]];
 
-const increasedPrice2 = prices
-    .map(product => product[1] - product[0])
-    .filter(price => price > 0);
+// acc - темп переменная куда складывается операция
+// value - текущее значение массива
+// i - номер операции
+const finalBalance = operations.reduce((acc,value, i) => {
+    console.log(`Операция ${i+1}, Текущий баланс: ${acc}, Операция: ${value}$`);
+    return acc += value;
+}, 0); // значение по умолчанию (аля let finalBalance = 0)
 
-console.log(increasedPrice2)
+console.log(`Итого: ${finalBalance}$`);
+
+
+const minElement = operations.reduce((min,value, i) => {
+    return min < value ? min: min = value;
+}, 0); // значение по умолчанию (аля let finalBalance = 0)
+
+console.log(`Минимальный элемент: ${minElement}$`);
