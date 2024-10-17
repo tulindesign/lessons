f6550ead191a55e57126f66455653ac83c6d3ed5
tulin.design <t.tulin@mail.ru>
2024-10-17 14:33:20 +0500

    Базовые методы строк

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-// вырезать из массива значения
-//variant 1
-function filterRangeInPlace(arr, a, b) {
-  for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < a || arr[i] > b) {
-            arr.splice(i,1);
-            i--;
-      } 
-    }
-  }
-const numbers = [1,3,5,9,2,10,24,13]
-console.log(numbers);
-filterRangeInPlace(numbers,3,9)
-console.log(numbers);
+// Базовые методы
+const userName = 'вася пупкина';
 
-//variant 2
-console.log('--------------')
-function filterRangeInPlace2(arr, a, b) {
-  tempArray = [];
-  for (const [index,item] of arr.entries()) {
-    if (item >= a && item <= b) {
-      tempArray.push(arr[index]);
-    } 
-  }
-  return arr.splice(0, arr.length, ...tempArray);
+function firstLetterToUpperCase (str) {
+  return str
+  .split(' ')
+  .map((item) => item[0].toUpperCase() + item.slice(1));
 }
-  
-let numbers2 = [1,3,5,9,2,10,24,13]
-console.log(numbers2);
-filterRangeInPlace2(numbers2,3,9);
-console.log(numbers2);
+console.log(firstLetterToUpperCase(userName)); 
+console.log(userName.charAt(0)); 
+
+
+console.log(userName.length); 
+console.log(userName.indexOf('уп'));
+console.log(userName.lastIndexOf('а'));
+console.log(userName.includes('а'));
+console.log(userName.slice(5,8));
+
+
 
