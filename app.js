108f893920f4a14a44a240e52f8a8064f7500fa2
tulin.design <t.tulin@mail.ru>
2024-10-18 22:53:43 +0500

    Упражнение преобразование объектов

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-// отсортировать пользователь по возрасту
+/*
+  преобразовать пользователей до вида 
+  { fullName: 'Вася Пуппкин', skillNum: 2}
+*/
 
-const users = [
-  { name: 'Вася', age:30},
-  { name: 'Катя', age:18},
-  { name: 'Аня', age:40},
-  { name: 'Петя', age:25},
+let users = [
+  {
+    name: 'Вася',
+    surname: 'Пупкин',
+    age: 30,
+    skills: ['Разработка', 'DevOps']
+  },
+  {
+    name: 'Катя',
+    surname: 'Белова',
+    age: 18,
+    skills: ['Дизайн']
+  }
 ]
-console.log(users);
-console.log(users.toSorted((a, b) => a.age - b.age));
+
+// users = users.map((item) => {
+//   item.fullName = `${item.name} ${item.surname}`,
+//   item.skillNum = item.skills.length;
+//   delete item.name;
+//   delete item.surname;
+//   delete item.age;
+//   delete item.skills;
+//   return {item};
+// })
+
+
+const userData = users.map(item => {
+    return {
+      fullName: `${item.name} ${item.surname}`,
+      skillNum: item.skills.length
+    };
+  })
+  
+console.log(userData);
