9b19f85370530b7b695037e149ffa1847e892f20
tulin.design <t.tulin@mail.ru>
2024-10-18 23:02:56 +0500

    Методы объектМетоды объектовов

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-/*
-  преобразовать пользователей до вида 
-  { fullName: 'Вася Пуппкин', skillNum: 2}
-*/
+/* Методы объектов */
 
-let users = [
-  {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 30,
-    skills: ['Разработка', 'DevOps']
-  },
-  {
-    name: 'Катя',
-    surname: 'Белова',
-    age: 18,
-    skills: ['Дизайн']
+const user = {
+  name: 'Вася',
+  surname: 'Пупин',
+  age: 24,
+  getFullName: function() {
+    return `${this.name} ${this.surname}`
   }
-]
-
-// users = users.map((item) => {
-//   item.fullName = `${item.name} ${item.surname}`,
-//   item.skillNum = item.skills.length;
-//   delete item.name;
-//   delete item.surname;
-//   delete item.age;
-//   delete item.skills;
-//   return {item};
-// })
-
-
-const userData = users.map(item => {
-    return {
-      fullName: `${item.name} ${item.surname}`,
-      skillNum: item.skills.length
-    };
-  })
-  
-console.log(userData);
+}
+console.log(user.getFullName());
