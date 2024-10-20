d4540511c19da7b68b4d12603e3e7074d5b02187
tulin.design <t.tulin@mail.ru>
2024-10-20 12:59:53 +0500

    Сокращение кода, можно использовать key: key -> key

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-/* Методы объектов */
-
-const user = {
-  name: 'Вася',
-  surname: 'Пупин',
-  age: 24,
-  getFullName: function() {
-    return `${this.name} ${this.surname}`
+/* 
+  Реализовать методы увеличения и уменьшения баланса, при котором каждая операция сохраняется в массиве operations в виде
+  {
+    reason: 'Оплата налогов', sum: -100}
   }
+    Возвращается true, если успешно и false, если не хватает баланса
+    Также реализовать метод вывода числа операций по кошельку
+*/
+
+const wallet = {
+  balance: 0,
+  operations: [],
+  increaseBalance(sum) {
+    this.balance += sum;
+    this.operations.push(
+      {
+        reason: 'Пополнение счета',
+        sum
+      }
+    );
+    return `${this.operations.at(-1).reason} на сумму ${this.operations.at(-1).sum}$. Баланс: ${this.balance}$`;
+  },
+  decreaseBalance(sum) {
+    if (this.balance >= Math.abs(sum)) {
+      this.balance -= Math.abs(sum);
+      this.operations.push(
+      {
+        reason: 'Оплата',
+        sum
+      }
+      );
+      return `${this.operations.at(-1).reason} на сумму ${this.operations.at(-1).sum}$. Баланс: ${this.balance}$`;
+    } else {
+      return `Не достаточно средств для операции ${sum}. Баланс: ${wallet.balance}$`
+    }
+    
+  }
+} 
+
+
+function addOperation(operationInput = +prompt('Введите сумма операции')) {
+  if (operationInput > 0) {
+    return wallet.increaseBalance(operationInput);
+  }
+  else {
+    return wallet.decreaseBalance(operationInput)
+  } 
 }
-console.log(user.getFullName());
+
+console.log(addOperation());
+console.log(addOperation());
