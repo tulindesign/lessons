244d172f4380e66ce92cf7e8990c96e2c3268dea
tulin.design <t.tulin@mail.ru>
2024-10-23 15:58:26 +0500

    call, apply

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 'use strict'
-// сокращенное написание методов в объектах
-const b = 1;
+// call, apply
 
-const a = {
-  b,
-  getBalt() {
-    return this.b
+const audi = {
+  mark: 'Audi',
+  model: 'A3',
+  year: 2021,
+  damages: [
+
+  ],
+  addDamage(part, rate) {
+    console.log(
+      `У авто ${this.mark}, ${this.model}, ${this.year} новое поврежедение:
+${part} со степенью ${rate}`
+  )
+    this.damages.push(
+      {
+        part,
+        rate
+      }
+    )
+    return this;
   }
 }
-console.log(a.getBalt());
+
+
+audi.addDamage('крыло','средняя').addDamage('капот','сильная').addDamage('дверь','легкая')
+
+
+const bmw = {
+  mark: 'BMW',
+  model: 'X5',
+  year: 2022,
+  damages: [
+
+  ],
+}
+
+bmw.addDamage = audi.addDamage;
+bmw.addDamage('Бампер', 2);
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc('Бампер', 2);
+
+addDamageFunc.call(bmw, 'Бампер', 2);
+// nameOfFunction.call(object, arguments);
+
+
+addDamageFunc.apply(bmw, ['Бампер', 2]);
