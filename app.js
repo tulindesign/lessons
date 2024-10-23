d8272f1c3c1190eba642cada5d20013451f3bffa
tulin.design <t.tulin@mail.ru>
2024-10-23 15:38:19 +0500

    EOL методы объектоа - сокращенное написание методов в объектах

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
 'use strict'
-/*
+// сокращенное написание методов в объектах
+const b = 1;
 
-Дополнить объекто методами для получения имени
-- компании
-- сео
-- сотрудникоа
-
-*/
-
-const company = {
-  name: 'ООО Агро',
-  employees: [
-    {
-      name: 'Света',
-      getName() {
-        return this.name
-      },
-    },
-    {
-      name: 'Игорь',
-      getName() {
-        return this.name
-      },
-    }
-  ],
-  ceo: {
-      name: 'Вася',
-      getName() {
-        return this.name
-      },
-  },
-  getName() {
-    return this.name
-  },
+const a = {
+  b,
+  getBalt() {
+    return this.b
+  }
 }
-console.log(company.getName());
-console.log(company.ceo.getName());
-console.log(company.employees.map(item => item.getName()));
+console.log(a.getBalt());
